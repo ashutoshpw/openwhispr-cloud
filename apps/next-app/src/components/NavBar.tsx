069e55033f15bbaf193cf8835adce873aa626dab
Ashutosh Kumar <research@w3dev.email>
2026-02-13 12:49:57 +0000

    tweak navbar placeholder copy

--- apps/next-app/src/components/NavBar.tsx
+++ apps/next-app/src/components/NavBar.tsx
@@ -29,17 +29,17 @@ const components: { title: string; href: string; description: string }[] = [
   {
     title: "Marketing Page",
     href: "/marketing-page",
-    description: "Write some wavy here to get them to click.",
+    description: "Write something catchy here to get them to click.",
   },
   {
     title: "Second Tab",
     href: "/",
-    description: "Write some wavy here to get them to click.",
+    description: "Write something catchy here to get them to click.",
   },
   {
     title: "Third Tab",
     href: "/",
-    description: "Write some wavy here to get them to click.",
+    description: "Write something catchy here to get them to click.",
   },
 ];
 
